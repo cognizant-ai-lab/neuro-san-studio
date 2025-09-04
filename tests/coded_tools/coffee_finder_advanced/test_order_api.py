8898fdd027e5942c5f8b39cf13ad0a89ba4dbe8a
Olivier Francon <olivier.francon@cognizant.com>
2025-09-04 16:21:15 -0700

    Fix coffee finder advanced order API tests

--- tests/coded_tools/coffee_finder_advanced/test_order_api.py
+++ tests/coded_tools/coffee_finder_advanced/test_order_api.py
@@ -26,7 +26,7 @@ class TestOrderAPI(TestCase):
         order = {"customer_name": "Olivier", "shop_name": OrderAPI.SHOP_1, "order_details": "Black coffee"}
         response_1 = order_api.invoke(args=order, sly_data={})
         expected_resp_1 = f"Order 101 placed successfully for Olivier at {OrderAPI.SHOP_1}. Details: Black coffee"
-        self.assertEqual(response_1, expected_resp_1)
+        self.assertEqual(expected_resp_1, response_1)
 
     def test_invoke_customer_name(self):
         """
@@ -36,13 +36,13 @@ class TestOrderAPI(TestCase):
         order_api = OrderAPI()
         order = {"shop_name": OrderAPI.SHOP_1, "order_details": "Black coffee"}
         response_1 = order_api.invoke(args=order, sly_data={})
-        expected_error_1 = "Error: Please provide a customer name for the order."
-        self.assertEqual(response_1, expected_error_1)
+        expected_error_1 = "Error: Please provide a valid customer name for the order."
+        self.assertEqual(expected_error_1, response_1)
 
-        sly_data = {"user_name": "Olivier"}
+        sly_data = {"username": "Olivier"}
         response_2 = order_api.invoke(args=order, sly_data=sly_data)
         expected_resp_2 = f"Order 101 placed successfully for Olivier at {OrderAPI.SHOP_1}. Details: Black coffee"
-        self.assertEqual(response_2, expected_resp_2)
+        self.assertEqual(expected_resp_2, response_2)
 
     def test_invoke_shop_name(self):
         """
@@ -52,7 +52,7 @@ class TestOrderAPI(TestCase):
         order = {"customer_name": "Olivier", "order_details": "Black coffee"}
         response_1 = order_api.invoke(args=order, sly_data={})
         expected_error_1 = "Error: Please provide the name of the shop for the order."
-        self.assertEqual(response_1, expected_error_1)
+        self.assertEqual(expected_error_1, response_1)
 
         order = {"customer_name": "Olivier", "shop_name": "Unknown shop", "order_details": "Black coffee"}
         response_2 = order_api.invoke(args=order, sly_data={})
@@ -68,14 +68,14 @@ class TestOrderAPI(TestCase):
         order = {"customer_name": "Olivier", "shop_name": OrderAPI.SHOP_1, "order_details": "Black coffee"}
         response_1 = order_api.invoke(args=order, sly_data={})
         expected_resp_1 = f"Order 101 placed successfully for Olivier at {OrderAPI.SHOP_1}. Details: Black coffee"
-        self.assertEqual(response_1, expected_resp_1)
+        self.assertEqual(expected_resp_1, response_1)
 
         order = {"customer_name": "Olivier", "shop_name": OrderAPI.SHOP_2, "order_details": "Black coffee"}
         response_2 = order_api.invoke(args=order, sly_data={})
         expected_resp_2 = f"Order 201 placed successfully for Olivier at {OrderAPI.SHOP_2}. Details: Black coffee"
-        self.assertEqual(response_2, expected_resp_2)
+        self.assertEqual(expected_resp_2, response_2)
 
         order = {"customer_name": "Olivier", "shop_name": OrderAPI.SHOP_3, "order_details": "Black coffee"}
         response_3 = order_api.invoke(args=order, sly_data={})
         expected_resp_3 = f"Order 301 placed successfully for Olivier at {OrderAPI.SHOP_3}. Details: Black coffee"
-        self.assertEqual(response_3, expected_resp_3)
+        self.assertEqual(expected_resp_3, response_3)
