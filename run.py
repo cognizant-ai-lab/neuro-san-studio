19378ef4ba6e9477ce656256b10628db8f959216
Olivier Francon <olivier.francon@cognizant.com>
2025-10-08 15:39:02 -0700

    #319 Reformat with black

--- run.py
+++ run.py
@@ -331,27 +331,21 @@ class NeuroSanRunner:
             if self.is_windows:
                 self.server_process.terminate()
             else:
-                os.killpg(
-                    os.getpgid(self.server_process.pid), signal.SIGKILL
-                )
+                os.killpg(os.getpgid(self.server_process.pid), signal.SIGKILL)
 
         if self.flask_webclient_process:
             print(f"Stopping WEB CLIENT (PID {self.flask_webclient_process.pid})...")
             if self.is_windows:
                 self.flask_webclient_process.terminate()
             else:
-                os.killpg(
-                    os.getpgid(self.flask_webclient_process.pid), signal.SIGKILL
-                )
+                os.killpg(os.getpgid(self.flask_webclient_process.pid), signal.SIGKILL)
 
         if self.nsflow_process:
             print(f"Stopping NSFLOW (PID {self.nsflow_process.pid})...")
             if self.is_windows:
                 self.nsflow_process.terminate()
             else:
-                os.killpg(
-                    os.getpgid(self.nsflow_process.pid), signal.SIGKILL
-                )
+                os.killpg(os.getpgid(self.nsflow_process.pid), signal.SIGKILL)
 
         sys.exit(0)
 
@@ -444,7 +438,9 @@ class NeuroSanRunner:
         # Set up signal handling for termination
         signal.signal(signal.SIGINT, self.signal_handler)  # Handle Ctrl+C
         if self.is_windows:
-            signal.signal(signal.SIGBREAK, self.signal_handler)  # Handle Ctrl+Break on Windows  # pylint: disable=useless-suppression,no-member
+            signal.signal(
+                signal.SIGBREAK, self.signal_handler
+            )  # Handle Ctrl+Break on Windows  # pylint: disable=useless-suppression,no-member
         else:
             signal.signal(signal.SIGTERM, self.signal_handler)  # Handle kill command (not available on Windows)
 
