e09db89812281331e820a9dc4f5ddb7e551602f6
Noravee Kanchanavatee <noraveek@gmail.com>
2025-11-12 10:41:17 -0800

    add sample queries param

--- coded_tools/agent_network_designer/persist_agent_network.py
+++ coded_tools/agent_network_designer/persist_agent_network.py
@@ -98,7 +98,7 @@ class PersistAgentNetwork(CodedTool):
 
         logger.info(">>>>>>>>>>>>>>>>>>>Create Agent Network>>>>>>>>>>>>>>>>>>")
         logger.info("Agent Network Name: %s", str(the_agent_network_name))
-
+        print(f"\n\n{args=}\n\n")
         # Get the persistor first, as that will determine how we want to assemble the agent network
         persistor: AgentNetworkPersistor = AgentNetworkPersistorFactory.create_persistor(args, WRITE_TO_FILE)
         assembler: AgentNetworkAssembler = persistor.get_assembler()
