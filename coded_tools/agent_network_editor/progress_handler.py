76da147bb0ad87ade68ad7a7de46c736ae26093f
Dan Fink <Daniel.Fink@cognizant.com>
2025-12-05 10:40:17 -0800

    Make more of a factory

--- coded_tools/agent_network_editor/progress_handler.py
+++ coded_tools/agent_network_editor/progress_handler.py
@@ -16,6 +16,8 @@
 
 from typing import Any
 
+from os import environ
+
 from neuro_san.interfaces.agent_progress_reporter import AgentProgressReporter
 
 from coded_tools.agent_network_editor.constants import AGENT_NETWORK_DEFINITION
@@ -24,22 +26,37 @@ from coded_tools.agent_network_editor.constants import AGENT_NETWORK_NAME
 
 class ProgressHandler:
     """
-    Common handler for progress reporting during the building of agent networks
+    Common handler for progress progress during the building of agent networks
     """
 
     @staticmethod
     async def report_progress(args: dict[str, Any], network_definition: dict[str, Any], name: str = None) -> None:
         """
-        Common handler for progress reporting during the building of agent networks
+        Common handler for progress progress during the building of agent networks
 
         :param args: The arguments dictionary for the calling CodedTool
         :param network_definition: The network definition dictionary
         :param name: The name of the agent network. If None, will not be reported in progress.
         """
         progress_reporter: AgentProgressReporter = args.get("progress_reporter")
+
+        use_network_definition: dict[str, Any] = network_definition
+
+        agent_progress_style: str = environ.get("AGENT_PROGRESS_STYLE", "internal")
+        if agent_progress_style == "connectivity":
+            # The idea here is that a multi-user MAUI server can turn on this env variable
+            # so that agent network progress progress is converted to connectivity-style data format
+            # that it already knows how to render.
+            use_network_definition = ProgressHandler._convert_to_connectivity_style(network_definition)
+
+        elif agent_progress_style == "internal":
+            # Report the internal structure used by Agent Network Designer and pals.
+            # This is what was used in the first iterations with nsflow.
+            use_network_definition: dict[str, Any] = network_definition
+
         progress: dict[str, Any] = {
             # Agent network definition with an added agent
-            AGENT_NETWORK_DEFINITION: network_definition
+            AGENT_NETWORK_DEFINITION: use_network_definition
         }
 
         # Optionally add agent network name
@@ -47,3 +64,8 @@ class ProgressHandler:
             progress[AGENT_NETWORK_NAME] = name
 
         await progress_reporter.async_report_progress(progress)
+
+    def _convert_to_connectivity_style(self, network_definition: dict[str, Any]) -> dict[str, Any]:
+
+        connectivity: dict[str, Any] = {}
+        return connectivity
