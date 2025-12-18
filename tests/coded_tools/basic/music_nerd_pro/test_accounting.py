84d53bf605548116c7d0434abd1de494d7ee1204
vincent.nguyen <vincent.nguyen@cognizant.com>
2025-12-17 18:00:27 -0800

    fixed pylint error

--- tests/coded_tools/basic/music_nerd_pro/test_accounting.py
+++ tests/coded_tools/basic/music_nerd_pro/test_accounting.py
@@ -16,7 +16,7 @@
 
 from unittest import TestCase
 
-from coded_tools.basic.music_nerd_pro.Accountant import Accountant
+from coded_tools.basic.music_nerd_pro.accountant import Accountant
 
 
 class TestAccounting(TestCase):
