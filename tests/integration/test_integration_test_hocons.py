231256ec08048d201a4db6dabc6ae5bfa98bb2dd
vincent.nguyen <vincent.nguyen@cognizant.com>
2026-01-08 21:20:28 -0800

    fixed the 6 digits generated by yubikey

--- tests/integration/test_integration_test_hocons.py
+++ tests/integration/test_integration_test_hocons.py
@@ -68,7 +68,7 @@ class TestIntegrationTestHocons(TestCase):
         """
         # Call the guts of the dynamic test driver.
         # This will expand the test_hocon file name from the expanded list to
-        # include the file basis implied by the __file__ and path_to_basis above.531911
+        # include the file basis implied by the __file__ and path_to_basis above.
 
         self.DYNAMIC.one_test_hocon(self, test_name, test_hocon)
 
