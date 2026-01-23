84b94aec3656c8fe0c28b43e267e0c84657a96bf
Dan Fink <Daniel.Fink@cognizant.com>
2026-01-23 11:30:04 -0800

    Add metadata for hello_world

--- tests/integration/test_integration_test_hocons.py
+++ tests/integration/test_integration_test_hocons.py
@@ -124,6 +124,7 @@ class TestIntegrationTestHocons(TestCase):
                 # These can be in any order.
                 # Ideally more basic functionality will come first.
                 # Barring that, try to stick to alphabetical order.
+                "experimental/copy_cat/copy_hello_world.hocon",
                 "experimental/mdap_decomposer/long_multiplication.hocon",
                 "experimental/mdap_decomposer/list_sorting.hocon",
                 # List more hocon files as they become available here.
