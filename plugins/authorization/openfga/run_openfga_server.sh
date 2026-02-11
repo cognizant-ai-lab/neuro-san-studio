93aa8c895a9bfb27b2a87edcbebd0e24bd4cde9e
Dan Fink <Daniel.Fink@cognizant.com>
2026-02-11 09:03:29 -0800

    Make run_openfga_server more generic

--- plugins/authorization/openfga/run_openfga_server.sh
+++ plugins/authorization/openfga/run_openfga_server.sh
@@ -3,32 +3,74 @@
 # Script to run a sample OpenFGA server from scratch using docker commands
 # This will reset any authorization database every time it is run.
 
+
+#
+# Env vars to control the behavior of this script
+#
+
+# This Docker network allows containers to talk to each other and ports on the main host,
+# but not any incoming traffic.  See https://docs.docker.com/engine/network/ for more info
 DOCKER_NETWORK="bridge"
-DATASTORE_URI="file:/home/nonroot/openfga.db"
+
+# Name of the docker volume where the database is stored across container instances
+VOLUME=neuro_san_openfga
+
+# The user for the mount
+CONTAINER_USER=nonroot
+
+# The mount for the volume
+MOUNT=/home/${CONTAINER_USER}
+
+# Type of datastore to use
+DATASTORE=sqlite
+
+# URI on the volume where the database is stored
+DATASTORE_URI="file:${MOUNT}/openfga.db"
+
+# Port for incoming traffic to the OpenFGA container-internal HTTP server outside of the container
+INCOMING_HTTP_PORT=8082
+
+# Name of the OpenFGA container
+CONTAINER_NAME=neuro_san_openfga
+
+
+# From  https://openfga.dev/docs/getting-started/setup-openfga/docker#step-by-step
+# Pull the OpenFGA image if needed
+docker pull openfga/openfga
+
+#
+# Clean up
+#
 
 # Remove any existing containers that might be left over from previous runs of this script
-CONTAINERS=$(docker ps -a --filter volume=openfga | grep -v CONTAINER | awk '{print $1}')
+CONTAINERS=$(docker ps -a --filter volume=${VOLUME} | grep -v CONTAINER | awk '{print $1}')
 if [ -n "${CONTAINERS}" ]
 then
     echo "${CONTAINERS}" | xargs docker rm -f
 fi
 
 # Remove any existing volumes that might be left over from previous runs of this script.
-docker volume rm -f openfga
+docker volume rm -f ${VOLUME}
+
+
+#
+# From https://openfga.dev/docs/getting-started/setup-openfga/docker#using-sqlite
+#
 
 # Create a fresh volume
-docker volume create openfga
+docker volume create ${VOLUME}
 
 # Run the server with SQLite migrations
 docker run --rm --network="${DOCKER_NETWORK}" \
-    -v openfga:/home/nonroot \
-    -u nonroot \
-    openfga/openfga migrate --datastore-engine sqlite --datastore-uri "${DATASTORE_URI}"
+    -v ${VOLUME}:${MOUNT} \
+    -u ${CONTAINER_USER} \
+    openfga/openfga migrate --datastore-engine ${DATASTORE} --datastore-uri "${DATASTORE_URI}"
 
 # Run the server to stay up
-# Note the translation of local port 8082 to the container port 8080 for the OpenFGA internal HTTP server
-docker run --name openfga --network="${DOCKER_NETWORK}" \
-    -p 3000:3000 -p 8082:8080 -p 8081:8081 \
-    -v openfga:/home/nonroot \
-    -u nonroot  \
-    openfga/openfga run --datastore-engine sqlite --datastore-uri "${DATASTORE_URI}"
+# Note the translation of local port INCOMING_HTTP_PORT to the container port 8080
+# for the OpenFGA internal HTTP server
+docker run --name ${CONTAINER_NAME} --network="${DOCKER_NETWORK}" \
+    -p 3000:3000 -p ${INCOMING_HTTP_PORT}:8080 -p 8081:8081 \
+    -v ${VOLUME}:${MOUNT} \
+    -u ${CONTAINER_USER}  \
+    openfga/openfga run --datastore-engine ${DATASTORE} --datastore-uri "${DATASTORE_URI}"
