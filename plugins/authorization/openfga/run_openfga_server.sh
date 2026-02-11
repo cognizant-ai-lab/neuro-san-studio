3146a178354384c5a412320b42b5f1e488b0d508
Dan Fink <Daniel.Fink@cognizant.com>
2026-02-10 20:57:43 -0800

    Repair script for no existing containers case

--- plugins/authorization/openfga/run_openfga_server.sh
+++ plugins/authorization/openfga/run_openfga_server.sh
@@ -4,23 +4,31 @@
 # This will reset any authorization database every time it is run.
 
 DOCKER_NETWORK="bridge"
+DATASTORE_URI="file:/home/nonroot/openfga.db"
 
 # Remove any existing containers that might be left over from previous runs of this script
-docker ps -a --filter volume=openfga | grep -v CONTAINER | awk '{print $1}' | xargs docker rm
+CONTAINERS=$(docker ps -a --filter volume=openfga | grep -v CONTAINER | awk '{print $1}')
+if [ -n "${CONTAINERS}" ]
+then
+    echo "${CONTAINERS}" | xargs docker rm -f
+fi
+
+# Remove any existing volumes that might be left over from previous runs of this script.
+docker volume rm -f openfga
 
 # Create a fresh volume
 docker volume create openfga
 
 # Run the server with SQLite migrations
-docker run --rm --network=${DOCKER_NETWORK} \
+docker run --rm --network="${DOCKER_NETWORK}" \
     -v openfga:/home/nonroot \
     -u nonroot \
-    openfga/openfga migrate --datastore-engine sqlite --datastore-uri 'file:/home/nonroot/openfga.db'
+    openfga/openfga migrate --datastore-engine sqlite --datastore-uri "${DATASTORE_URI}"
 
 # Run the server to stay up
 # Note the translation of local port 8082 to the container port 8080 for the OpenFGA internal HTTP server
-docker run --name openfga --network=${DOCKER_NETWORK} \
+docker run --name openfga --network="${DOCKER_NETWORK}" \
     -p 3000:3000 -p 8082:8080 -p 8081:8081 \
     -v openfga:/home/nonroot \
     -u nonroot  \
-    openfga/openfga run --datastore-engine sqlite --datastore-uri 'file:/home/nonroot/openfga.db'
+    openfga/openfga run --datastore-engine sqlite --datastore-uri "${DATASTORE_URI}"
