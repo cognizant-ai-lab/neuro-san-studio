abf23a1d0be7ea4ef32f68902b28e0cca907b4f0
vincent.nguyen <vincent.nguyen@cognizant.com>
2026-03-25 15:22:15 -0700

    removed copyright from fixture test file

--- coded_tools/agent_network_test_generator/persist_test_fixture.py
+++ coded_tools/agent_network_test_generator/persist_test_fixture.py
@@ -23,25 +23,6 @@ from typing import Union
 
 from neuro_san.interfaces.coded_tool import CodedTool
 
-# The license header to prepend to every generated HOCON file.
-_LICENSE_HEADER: str = """\
-# Copyright (C) 2025-2026 Cognizant Technology Solutions Corp, www.cognizant.com.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# END COPYRIGHT
-"""
-
 _REFERENCE_COMMENT: str = """\
 
 # This file defines everything necessary for a data-driven test.
@@ -114,7 +95,7 @@ class PersistTestFixture(CodedTool):
 
         # Build the file content
         fixture_json: str = json.dumps(test_fixture, indent=4, ensure_ascii=False)
-        content: str = _LICENSE_HEADER + _REFERENCE_COMMENT + "\n" + fixture_json + "\n"
+        content: str = _REFERENCE_COMMENT.lstrip("\n") + "\n" + fixture_json + "\n"
 
         try:
             with open(output_path, "w", encoding="utf-8") as fout:
